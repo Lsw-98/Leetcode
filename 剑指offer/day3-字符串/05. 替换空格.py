7a705af1f65d95dcbf218d4c1d79f9f1381ea650
lsw <454412569@qq.om>
2022-03-02 11:00:39 +0800

    剑指offer day3

---
+++ 剑指offer/day3-字符串/05. 替换空格.py
@@ -0,0 +1,17 @@
+# 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+
+# 示例 1：
+
+# 输入：s = "We are happy."
+# 输出："We%20are%20happy."
+
+
+def replaceSpace(s):
+  res = list(s)
+  for i in range(len(res)):
+    if res[i] == " ":
+      res[i] = "%20"
+  return ''.join(res)
+
+
+print(replaceSpace("We are happy."))
